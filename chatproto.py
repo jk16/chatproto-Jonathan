fa39099bd3fb17887904cbdfaec7767d51490993
jk16 <dude16kh@gmail.com>
2016-01-18 20:30:01 -0500

    Initial Commit

--- chatproto.py
+++ chatproto.py
@@ -22,6 +22,42 @@
 
 
 
+def main():
+	pass 
+
+
+
+
+
+if __name__ == "__main__":
+	"""
+	Purpose: If you use as a module code wont get executed
+			 If you use it as an executable it will get called 
+	"""
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
