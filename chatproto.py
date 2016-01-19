87f4f3cc2d3c771d613e3a941c42368c90bfbf1d
jk16 <dude16kh@gmail.com>
2016-01-18 21:07:02 -0500

    Implemented Tornado hello world setup

--- chatproto.py
+++ chatproto.py
@@ -1,40 +1,32 @@
+import tornado.ioloop
+import tornado.web
 
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
 
 
 
 def main():
-	pass 
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
 
 
 
 
 
 if __name__ == "__main__":
-	"""
-	Purpose: If you use as a module code wont get executed
-			 If you use it as an executable it will get called 
-	"""
-	main()
+    """
+    Purpose: If you use as a module code wont get executed
+             If you use it as an executable it will get called 
+    """
+    main()
 
 
 
@@ -43,7 +35,7 @@ if __name__ == "__main__":
 
 
 
-
+    
 
 
 
