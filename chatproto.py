1c8424fb1a3412ec4adc58e97f472b493fbc0812
jk16 <dude16kh@gmail.com>
2016-01-19 20:46:40 -0500

    Layout/ChatPageHandler
    * Added project layout
    * Changed MainHandler to ChatPageHandler
    * Added html template to ChatPageHandler

--- chatproto.py
+++ chatproto.py
@@ -1,14 +1,50 @@
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+"""
+* Client:
+    * Chat Page
+        * Message Buffer
+            * Watch for new messages
+        * Login Form
+            * Nick name
+            * Submit button
+            * Submits data via ajax
+            * If login is successful, will show Input Form
+        * Input Form
+            * Replaces Login Form after successful login
+            * Input box for new messages
+            * Submit button to send messages
+        * Nick Buffer
+            * Shows all chatters' nicks
+            * When someone new joins, nick shows up here
+            * When someone logs out or disconnects, nick goes away from here
+* Server
+    * Serves Chat Page
+    * Answer login queries
+    * Take message submissions
+    * Rebroadcast message submissions
+    * Rebroadcast nick logins/logouts
+"""
+
+class ChatPageHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        """
+        GET request from browser
+        """
+
+
+
+
+        self.render('chatpage.html')
+
+        
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+    handlers = [
+        (r"/", ChatPageHandler),
+    ]
+    return tornado.web.Application(handlers,debug=True,template_path='./templates')
 
 
 
